fc795333953548c7f158306e9b383e1ed382a455
kreivanser <yasyasyas1233212409999123@mail.ru>
2023-12-25 06:15:01 +0500

    com 2

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-def bubble_sort(arr):
+def bubble_sort(arr, order):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if order == "возрастанию":
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+            elif order == "убыванию":
+                if arr[j] < arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
 
 num = int(input("Введите количество чисел: "))
+order = input("Хотите отсортировать числа по возрастанию или убыванию? ")
 numbers = [int(input(f"Введите число #{i+1}: ")) for i in range(num)]
 
-bubble_sort(numbers)
-print("Отсортированный массив по возрастанию:", numbers)
-
+bubble_sort(numbers, order)
+print(f"Отсортированный массив по {order}:", numbers)
